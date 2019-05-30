c376b684669b5242904d10b7ee86bccdb95bb6fe
Susmita21197 <choudhurysusmita70>
2019-05-30 05:43:10 -0700

    updated

--- Demo/src/hey/Susmita.java
+++ Demo/src/hey/Susmita.java
@@ -3,17 +3,5 @@ package hey;
 public class Susmita {
  //everyone create a class on their name
 	
-	
-	
-	
-}
-public class Ajith
-{
-	
-	//I have my project.....
-}
 
-public class Kiran
-{
-	//Finally I Got It
 }
